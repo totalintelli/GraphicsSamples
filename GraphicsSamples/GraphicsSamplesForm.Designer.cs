9221f7c7d2128d00b2e356c31ab1b6f9febcc4af
DEVZONEYDS\USER <kwanwoo5@naver.com>
2017-12-27 12:42:48 +0900

    first commit

---
+++ GraphicsSamples/GraphicsSamplesForm.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace GraphicsSamples
+{
+    partial class GraphicsSamplesForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRotateTransform = new System.Windows.Forms.Button();
+            this.btnSetClipPathCombine = new System.Windows.Forms.Button();
+            this.btnDrawString = new System.Windows.Forms.Button();
+            this.btnSetAndFillClip = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnRotateTransform
+            // 
+            this.btnRotateTransform.Location = new System.Drawing.Point(12, 444);
+            this.btnRotateTransform.Name = "btnRotateTransform";
+            this.btnRotateTransform.Size = new System.Drawing.Size(113, 23);
+            this.btnRotateTransform.TabIndex = 0;
+            this.btnRotateTransform.Text = "RotateTransform";
+            this.btnRotateTransform.UseVisualStyleBackColor = true;
+            this.btnRotateTransform.Click += new System.EventHandler(this.btn_Clicked);
+            // 
+            // btnSetClipPathCombine
+            // 
+            this.btnSetClipPathCombine.Location = new System.Drawing.Point(131, 444);
+            this.btnSetClipPathCombine.Name = "btnSetClipPathCombine";
+            this.btnSetClipPathCombine.Size = new System.Drawing.Size(139, 23);
+            this.btnSetClipPathCombine.TabIndex = 1;
+            this.btnSetClipPathCombine.Text = "SetClipPathCombine";
+            this.btnSetClipPathCombine.UseVisualStyleBackColor = true;
+            this.btnSetClipPathCombine.Click += new System.EventHandler(this.btn_Clicked);
+            // 
+            // btnDrawString
+            // 
+            this.btnDrawString.Location = new System.Drawing.Point(277, 444);
+            this.btnDrawString.Name = "btnDrawString";
+            this.btnDrawString.Size = new System.Drawing.Size(75, 23);
+            this.btnDrawString.TabIndex = 2;
+            this.btnDrawString.Text = "DrawString";
+            this.btnDrawString.UseVisualStyleBackColor = true;
+            // 
+            // btnSetAndFillClip
+            // 
+            this.btnSetAndFillClip.Location = new System.Drawing.Point(359, 444);
+            this.btnSetAndFillClip.Name = "btnSetAndFillClip";
+            this.btnSetAndFillClip.Size = new System.Drawing.Size(95, 23);
+            this.btnSetAndFillClip.TabIndex = 3;
+            this.btnSetAndFillClip.Text = "SetAndFillClip";
+            this.btnSetAndFillClip.UseVisualStyleBackColor = true;
+            // 
+            // GraphicsSamplesForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(686, 479);
+            this.Controls.Add(this.btnSetAndFillClip);
+            this.Controls.Add(this.btnDrawString);
+            this.Controls.Add(this.btnSetClipPathCombine);
+            this.Controls.Add(this.btnRotateTransform);
+            this.Name = "GraphicsSamplesForm";
+            this.Text = "그래픽 샘플들";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.GraphicsSamplesForm_Paint);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRotateTransform;
+        private System.Windows.Forms.Button btnSetClipPathCombine;
+        private System.Windows.Forms.Button btnDrawString;
+        private System.Windows.Forms.Button btnSetAndFillClip;
+    }
+}
+
---
+++ GraphicsSamples/GraphicsSamplesForm.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GraphicsSamples
+{
+    public partial class GraphicsSamplesForm : Form
+    {
+        bool btnRotateTransformClicked = false;    // RotateTransform 버튼이 클릭되었는지 여부
+        bool btnSetClipPathCombineClicked = false; // SetClipPathCombine 버튼이 클릭되었는지 여부
+        bool btnDrawStringClicked = false;         // DrawString 버튼이 클릭되었는지 여부 
+        bool btnSetAndFillClipClicked = false;              // 초기화 버튼이 클릭되었는지 여부
+
+        public GraphicsSamplesForm()
+        {
+            InitializeComponent();
+            btnRotateTransformClicked = false;
+            btnSetClipPathCombineClicked = false;
+            btnDrawStringClicked = false;
+            btnSetAndFillClipClicked = false;
+        }
+
+        private void btn_Clicked(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+
+            if(btn == btnRotateTransform)
+            {
+                btnRotateTransformClicked = true;
+
+            }
+            else if(btn == btnSetClipPathCombine)
+            {
+                btnSetClipPathCombineClicked = true;
+            }
+            else if(btn == btnDrawString)
+            {
+                btnDrawStringClicked = true;
+            }
+            else if(btn == btnSetAndFillClip)
+            {
+                btnSetAndFillClipClicked = true;
+            }
+
+            Invalidate();
+        }
+
+        private void GraphicsSamplesForm_Paint(object sender, PaintEventArgs e)
+        {
+            if(btnRotateTransformClicked)
+            {
+                RotateTransformAngle(e);
+            }
+            else if(btnSetClipPathCombineClicked)
+            {
+                SetClipPathCombine(e);
+            }
+            else if(btnDrawStringClicked)
+            {
+                DrawStringPointF(e);
+            }
+            else if(btnSetAndFillClipClicked)
+            {
+                SetAndFillClip(e);
+            }
+
+            BeginContainerVoid(e);
+
+            btnRotateTransformClicked = false;
+            btnSetClipPathCombineClicked = false;
+            btnDrawStringClicked = false;
+            btnSetAndFillClipClicked = false;
+        }
+
+        private void RotateTransformAngle(PaintEventArgs e)
+        {
+
+            // Set world transform of graphics object to translate.
+            e.Graphics.TranslateTransform(100.0F, 0.0F);
+
+            // Then to rotate, prepending rotation matrix.
+            e.Graphics.RotateTransform(30.0F);
+
+            // Draw rotated, translated ellipse to screen.
+            e.Graphics.DrawEllipse(new Pen(Color.Blue, 3), 0, 0, 200, 80);
+        }
+
+        private void SetClipPathCombine(PaintEventArgs e)
+        {
+
+            // Create graphics path.
+            GraphicsPath clipPath = new GraphicsPath();
+            clipPath.AddEllipse(0, 0, 200, 100);
+
+            // Set clipping region to path. 
+            //e.Graphics.SetClip(clipPath, CombineMode.Replace); // CombineMode.Replace : 클립 영역에 있는 부분만 출력
+            e.Graphics.SetClip(clipPath, CombineMode.Exclude);   // CombineMode.Exclude : 클립 영역을 제외한 부분을 출력
+
+            // Fill rectangle to demonstrate cliping region
+            e.Graphics.FillRectangle(new SolidBrush(Color.Black), 0, 0, 500, 300);
+
+            btnSetClipPathCombineClicked = false;
+        }
+
+        private void DrawStringPointF(PaintEventArgs e)
+        {
+
+            // Create string to draw.
+            String drawString = "Sample Text";
+
+            // Create font and brush.
+            Font drawFont = new Font("Arial", 16);
+            SolidBrush drawBrush = new SolidBrush(Color.Black);
+
+            // Create point for upper-left corner of drawing.
+            PointF drawPoint = new PointF(0.0F, 0.0F);
+
+            // Draw string to screen.
+            e.Graphics.DrawString(drawString, drawFont, drawBrush, drawPoint);
+        }
+
+        private void SetAndFillClip(PaintEventArgs e)
+        {
+
+            // Set the Clip property to a new region.
+            e.Graphics.Clip = new Region(new Rectangle(10, 10, 100, 200));
+
+            // Fill the region.
+            e.Graphics.FillRegion(Brushes.LightSalmon, e.Graphics.Clip);
+
+            // Demonstrate the clip region by drawing a string
+            // at the outer edge of the region.
+            e.Graphics.DrawString("Outside of Clip", new Font("Arial", 
+                12.0F, FontStyle.Regular), Brushes.Black, 0.0F, 0.0F); 
+        }
+
+        private void BeginContainerVoid(PaintEventArgs e)
+        {
+            // Begin graphics container.
+            GraphicsContainer containerState = e.Graphics.BeginContainer();
+
+            // Translate workd transformation.
+            e.Graphics.TranslateTransform(100.0F, 100.0F);
+
+            // Fill translated rectangle in container with red.
+            e.Graphics.FillRectangle(new SolidBrush(Color.Red), 0, 0, 200, 200);
+
+            // End graphics container.
+            e.Graphics.EndContainer(containerState);
+
+            // Fill untransformed rectangle with green.
+            e.Graphics.FillRectangle(new SolidBrush(Color.Green), 0, 0, 200, 200);
+        }
+    }
+}
